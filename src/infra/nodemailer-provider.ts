5ece19d95d50c3a6118ed14e08c5380e5cead024
“Breno <brenorodriguesdev@gmail.com>
2022-01-09 16:27:48 -0300

    feat: implements mail provider

---
+++ src/infra/nodemailer-provider.ts
@@ -0,0 +1,32 @@
+import nodemailer from 'nodemailer'
+import Mail from 'nodemailer/lib/mailer'
+import { MailProvider, Message } from '../data/contracts/mail-provider'
+
+export class NodemailerProvider implements MailProvider {
+  private readonly transporter: Mail
+
+  constructor () {
+    this.transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.USER,
+        pass: process.env.PASSWORD
+      }
+    })
+  }
+
+  async sendMail (message: Message): Promise<void> {
+    await this.transporter.sendMail({
+      to: {
+        name: message.to.name,
+        address: message.to.email
+      },
+      from: {
+        name: message.from.name,
+        address: message.from.email
+      },
+      subject: message.subject,
+      html: message.body
+    })
+  }
+}
